265c35eeeada6478d7266c8cfb925686369382af
Noah Simcox <nssimcox@gmail.com>
2023-11-07 16:40:44 -0500

    stuff

---
+++ Assets/Scripts/PlayerJumpController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerJumpController : MonoBehaviour
+{
+    private CharacterController _cc;
+    [SerializeField] private float jumpHeight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _cc = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _cc.SimpleMove(Vector3.up * jumpHeight);
+        }
+    }
+}
