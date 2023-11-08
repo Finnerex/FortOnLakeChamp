124084167f88fb3ac59d69e4fe071c07a253f5d5
Noah Simcox <nssimcox@gmail.com>
2023-11-07 19:21:52 -0500

    shoot

--- Assets/Scripts/Shoot.cs
+++ Assets/Scripts/Shoot.cs
@@ -16,11 +16,12 @@ public class Shoot : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Transform playerTransform = transform;
         Vector3 musketBallVector = Vector3.zero;
         
         if (Input.GetMouseButton(0) && _musket.IsHeld)
         {
-            Instantiate(musketBall);
+            Instantiate(musketBall, transform.position, Quaternion.identity);
             musketBall.velocity = transform.forward * 10;
         }
     }
