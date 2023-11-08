4d74adc6fe13cb96f3289bada061cd0e616d5e53
NoahSimcox <nssimcox@gmail.com>
2023-11-08 00:41:27 -0500

    added reload timer and reload text

--- Assets/Scripts/Shoot.cs
+++ Assets/Scripts/Shoot.cs
@@ -2,10 +2,15 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Shoot : MonoBehaviour
 {
+    [SerializeField] Text text;
     private PickubableObject _musket;
+    private bool _canShoot = true; // Variable to control shooting cooldown
+
+    [SerializeField] private float speed;
     [SerializeField] private Rigidbody musketBall;
 
     void Start()
@@ -16,12 +21,38 @@ public class Shoot : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Transform playerTransform = transform;
-        
-        if (Input.GetMouseButtonDown(0) && _musket.IsHeld)
+        if (Input.GetMouseButtonDown(0) && _musket.IsHeld && _canShoot)
         {
-            Instantiate(musketBall, playerTransform.position, Quaternion.identity);
-            musketBall.AddForce(playerTransform.forward * 1000);
+            // Trigger the shooting coroutine
+            StartCoroutine(ShootCoroutine());
+
+            // Prevent shooting until the cooldown is over
+            _canShoot = false;
         }
+    }
+
+    // Coroutine for the shooting logic
+    private IEnumerator ShootCoroutine()
+    {
+        Transform playerTransform = transform;
+        Vector3 playerForward = -playerTransform.forward;
+        Vector3 ballOffset = playerForward * 1.5f + playerTransform.up * 1.35f;
+        Instantiate(musketBall, playerTransform.position + ballOffset, Quaternion.identity).velocity =
+            (playerForward * speed);
+        
+        //enables reload text
+        text.enabled = true;
+        
+        //garenteed 3 sec wait time
+        yield return new WaitForSeconds(3f);
+        
+        //waits until 'r' is pressed
+        yield return new WaitUntil(() => Input.GetKeyDown(KeyCode.R));
+        
+        //gets rid of reload text
+        text.enabled = false;
+        
+        // Allow shooting again
+        _canShoot = true;
     }
 }
---
+++ Assets/Scripts/TextController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class TextController : MonoBehaviour
+{
+    public Text textComponent; // Reference to the Text component in the Inspector
+
+    // Call this method to activate the text
+    public void ActivateText(string newText)
+    {
+        textComponent.text = newText;
+        textComponent.enabled = true;
+    }
+
+    // Call this method to deactivate the text
+    public void DeactivateText()
+    {
+        textComponent.enabled = false;
+    }
+}
