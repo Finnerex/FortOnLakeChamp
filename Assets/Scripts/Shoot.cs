b9744ea9c154f0c1c99f29118a3408bbc76aa990
Noah Simcox <nssimcox@gmail.com>
2023-11-07 19:11:16 -0500

    shoot

--- Assets/Scripts/Shoot.cs
+++ Assets/Scripts/Shoot.cs
@@ -1,23 +1,27 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Shoot : MonoBehaviour
 {
-    
+    private PickubableObject _musket;
     [SerializeField] private Rigidbody musketBall;
-    
+
+    void Start()
+    {
+        _musket = GetComponent<PickubableObject>();
+    }
+
     // Update is called once per frame
     void Update()
     {
-        Transform ballTransform = transform;
         Vector3 musketBallVector = Vector3.zero;
         
-        if (Input.GetMouseButton(0))
+        if (Input.GetMouseButton(0) && _musket.IsHeld)
         {
             Instantiate(musketBall);
-            musketBallVector = ballTransform.forward * 10;
+            musketBall.velocity = transform.forward * 10;
         }
-        musketBall.velocity = musketBallVector;
     }
 }
