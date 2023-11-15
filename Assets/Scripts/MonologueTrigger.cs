8b77c1787f2029124fd7f3d4e64bdaf25fb1a419
Finnerex <61033009+Finnerex@users.noreply.github.com>
2023-11-15 16:58:08 -0500

    monologues and ship

---
+++ Assets/Scripts/MonologueTrigger.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+
+public class MonologueTrigger : MonoBehaviour
+{
+
+    [TextArea] [SerializeField] private string text;
+    [SerializeField] private float displayTimeSeconds = 5;
+    
+    [SerializeField] private PlayerSpeech speechController;
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<PlayerController>() == null)
+            return;
+        
+        speechController.SetMonologue(text, displayTimeSeconds);
+    }
+}
